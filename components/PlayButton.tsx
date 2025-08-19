2a035696027e943c1b8f5ea061b8fb1d74045886
Abhi <abhinav118@gmail.com>
2025-08-19 17:37:01 -0400

    Add components/PlayButton.tsx

---
+++ components/PlayButton.tsx
@@ -0,0 +1,12 @@
+interface PlayButtonProps {
+  variant?: any
+  size?: any
+}
+
+export default function PlayButton({ variant, size }: PlayButtonProps) {
+  return (
+    <div className="">
+      {/* PlayButton component content */}
+    </div>
+  )
+}
