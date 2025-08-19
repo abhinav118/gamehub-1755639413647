fb260854124af417f8fe59ab0c95b1706f5d6ac0
Abhi <abhinav118@gmail.com>
2025-08-19 17:37:02 -0400

    Add components/PlayerAvatar.tsx

---
+++ components/PlayerAvatar.tsx
@@ -0,0 +1,12 @@
+interface PlayerAvatarProps {
+  circular?: any
+  size?: any
+}
+
+export default function PlayerAvatar({ circular, size }: PlayerAvatarProps) {
+  return (
+    <div className="">
+      {/* PlayerAvatar component content */}
+    </div>
+  )
+}
