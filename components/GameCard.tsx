5c15eb2dd3fb2b12d49152202bcd5eb754bff4da
Abhi <abhinav118@gmail.com>
2025-08-19 17:37:01 -0400

    Add components/GameCard.tsx

---
+++ components/GameCard.tsx
@@ -0,0 +1,12 @@
+interface GameCardProps {
+  hoverable?: any
+  clickable?: any
+}
+
+export default function GameCard({ hoverable, clickable }: GameCardProps) {
+  return (
+    <div className="">
+      {/* GameCard component content */}
+    </div>
+  )
+}
